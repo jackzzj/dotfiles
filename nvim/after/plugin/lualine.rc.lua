da7058e9d76b41f8afb4c8c9a585cc4c7e388336
Zhan Kaijie <ceres0503@icloud.com>
2022-08-23 15:47:09 -0400

    update

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -3,49 +3,41 @@ if (not status) then return end
 
 lualine.setup {
   options = {
-    icons_enabled = false,
+    icons_enabled = true,
     theme = 'solarized_dark',
-    section_separators = {left = '', right = ''},
-    component_separators = {left = '', right = ''},
+    section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
     disabled_filetypes = {}
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {{
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
       path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    }},
+    } },
     lualine_x = {
-      { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
+      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+        hint = ' ' } },
       'encoding',
       'filetype'
     },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {{
+    lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
       path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
-    }},
-    lualine_x = {'location'},
+    } },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {
-    lualine_a = {'buffers'},
-    lualine_b = {},
-    lualine_c = {{
-    }},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  extensions = {'fugitive'}
+  tabline = {},
+  extensions = { 'fugitive' }
 }
-
